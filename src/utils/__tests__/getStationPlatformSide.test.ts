cd8b9499eb14beb75b73388b6ca235cbd605f552
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2023-06-05 17:22:46 +0300

    Fix getStationPlatformSide tests

--- src/utils/__tests__/getStationPlatformSide.test.ts
+++ src/utils/__tests__/getStationPlatformSide.test.ts
@@ -1,4 +1,7 @@
-import { TrainDetailsFragment } from '../../graphql/generated/digitraffic';
+import {
+  StationPlatformSide,
+  TrainDetailsFragment,
+} from '../../graphql/generated/digitraffic';
 import getStationPlatformSide from '../getStationPlatformSide';
 import getTimeTableRowsGroupedByStation from '../getTimeTableRowsGroupedByStation';
 import getTrainDirection from '../getTrainDirection';
@@ -35,21 +38,21 @@ describe('getTrainPlatformSide', () => {
   describe('U train HKI-KKN', () => {
     const train = trainUHkiKknSingleUnit as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'right', // Helsinki - 14
-      'right', // Pasila - 8
-      'left', // Huopalahti - 1
-      'right', // Leppävaara - 1
-      'right', // Kilo - 1
-      'right', // Kera - 1
-      'right', // Kauniainen - 1
-      'right', // Koivuhovi - 1
-      'right', // Tuomarila - 1
-      'right', // Espoo - 1
-      'right', // Kauklahti - 1
-      'right', // Masala - 1
-      'right', // Jorvas - 1
-      'right', // Tolsa - 1
-      'right', // Kirkkunummi - 1
+      StationPlatformSide.Right, // Helsinki - 14
+      StationPlatformSide.Right, // Pasila - 8
+      StationPlatformSide.Left, // Huopalahti - 1
+      StationPlatformSide.Right, // Leppävaara - 1
+      StationPlatformSide.Right, // Kilo - 1
+      StationPlatformSide.Right, // Kera - 1
+      StationPlatformSide.Right, // Kauniainen - 1
+      StationPlatformSide.Right, // Koivuhovi - 1
+      StationPlatformSide.Right, // Tuomarila - 1
+      StationPlatformSide.Right, // Espoo - 1
+      StationPlatformSide.Right, // Kauklahti - 1
+      StationPlatformSide.Right, // Masala - 1
+      StationPlatformSide.Right, // Jorvas - 1
+      StationPlatformSide.Right, // Tolsa - 1
+      StationPlatformSide.Right, // Kirkkunummi - 1
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
@@ -58,21 +61,21 @@ describe('getTrainPlatformSide', () => {
   describe('U train KKN-HKI', () => {
     const train = trainUKknHkiSingleUnit as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'left', // Kirkkonummi - 3
-      'right', // Tolsa - 2
-      'right', // Jorvas - 2
-      'right', // Masala - 2
-      'right', // Kauklahti - 2
-      'right', // Espoo - 2
-      'right', // Tuomarila - 2
-      'right', // Koivuhovi - 2
-      'right', // Kauniainen - 2
-      'right', // Kera - 2
-      'right', // Kilo - 2
-      'right', // Leppävaara - 2
-      'left', // Huopalahti - 2
-      'right', // Pasila - 9
-      'left', // Helsinki - 14
+      StationPlatformSide.Left, // Kirkkonummi - 3
+      StationPlatformSide.Right, // Tolsa - 2
+      StationPlatformSide.Right, // Jorvas - 2
+      StationPlatformSide.Right, // Masala - 2
+      StationPlatformSide.Right, // Kauklahti - 2
+      StationPlatformSide.Right, // Espoo - 2
+      StationPlatformSide.Right, // Tuomarila - 2
+      StationPlatformSide.Right, // Koivuhovi - 2
+      StationPlatformSide.Right, // Kauniainen - 2
+      StationPlatformSide.Right, // Kera - 2
+      StationPlatformSide.Right, // Kilo - 2
+      StationPlatformSide.Right, // Leppävaara - 2
+      StationPlatformSide.Left, // Huopalahti - 2
+      StationPlatformSide.Right, // Pasila - 9
+      StationPlatformSide.Left, // Helsinki - 14
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
@@ -81,25 +84,25 @@ describe('getTrainPlatformSide', () => {
   describe('R train HKI-TPE', () => {
     const train = trainR4Units2FirstUnitsContinueFromRi as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'left', // Helsinki - 5
-      'right', // Pasila - 4
-      'right', // Tikkurila - 2
-      'right', // Kerava - 4
-      'right', // Ainola - 2
-      'left', // Järvenpää - 4
-      'left', // Saunakallio - 4
-      'right', // Jokela - 2
-      'right', // Hyvinkää - 4
-      'right', // Riihimäki - 4
-      'right', // Ryttylä - 2
-      'right', // Turenki - 3
-      'left', // Hämeenlinna - 3
-      'left', // Parola - 3
-      'right', // Iittala - 2
-      'left', // Toijala - 4
-      'right', // Viiala - 2
-      'right', // Lempäälä - 3
-      'right', // Tampere - 2
+      StationPlatformSide.Left, // Helsinki - 5
+      StationPlatformSide.Right, // Pasila - 4
+      StationPlatformSide.Right, // Tikkurila - 2
+      StationPlatformSide.Right, // Kerava - 4
+      StationPlatformSide.Right, // Ainola - 2
+      StationPlatformSide.Left, // Järvenpää - 4
+      StationPlatformSide.Left, // Saunakallio - 4
+      StationPlatformSide.Right, // Jokela - 2
+      StationPlatformSide.Right, // Hyvinkää - 4
+      StationPlatformSide.Right, // Riihimäki - 4
+      StationPlatformSide.Right, // Ryttylä - 2
+      StationPlatformSide.Right, // Turenki - 3
+      StationPlatformSide.Left, // Hämeenlinna - 3
+      StationPlatformSide.Left, // Parola - 3
+      StationPlatformSide.Right, // Iittala - 2
+      StationPlatformSide.Left, // Toijala - 4
+      StationPlatformSide.Right, // Viiala - 2
+      StationPlatformSide.Right, // Lempäälä - 3
+      StationPlatformSide.Right, // Tampere - 2
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
@@ -108,25 +111,25 @@ describe('getTrainPlatformSide', () => {
   describe('R train TPE-HKI', () => {
     const train = trainR2Units2AdditionalUnitsFromRi as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'right', // Tampere - 3
-      'right', // Lempäälä - 1
-      'right', // Viiala - 1
-      'left', // Toijala - 3
-      'right', // Iittala - 1
-      'right', // Parola - 1
-      'right', // Hämeenlinna - 1
-      'right', // Turenki - 1
-      'right', // Ryttylä - 1
-      'right', // Riihimäki - 1
-      'right', // Hyvinkää - 1
-      'right', // Jokela - 1
-      'right', // Saunakallio - 1
-      'right', // Järvenpää - 1
-      'right', // Ainola - 1
-      'right', // Kerava - 1
-      'right', // Tikkurila - 3
-      'right', // Pasila - 5
-      'right', // Helsinki - 5
+      StationPlatformSide.Right, // Tampere - 3
+      StationPlatformSide.Right, // Lempäälä - 1
+      StationPlatformSide.Right, // Viiala - 1
+      StationPlatformSide.Left, // Toijala - 3
+      StationPlatformSide.Right, // Iittala - 1
+      StationPlatformSide.Right, // Parola - 1
+      StationPlatformSide.Right, // Hämeenlinna - 1
+      StationPlatformSide.Right, // Turenki - 1
+      StationPlatformSide.Right, // Ryttylä - 1
+      StationPlatformSide.Right, // Riihimäki - 1
+      StationPlatformSide.Right, // Hyvinkää - 1
+      StationPlatformSide.Right, // Jokela - 1
+      StationPlatformSide.Right, // Saunakallio - 1
+      StationPlatformSide.Right, // Järvenpää - 1
+      StationPlatformSide.Right, // Ainola - 1
+      StationPlatformSide.Right, // Kerava - 1
+      StationPlatformSide.Right, // Tikkurila - 3
+      StationPlatformSide.Right, // Pasila - 5
+      StationPlatformSide.Right, // Helsinki - 5
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
@@ -135,15 +138,15 @@ describe('getTrainPlatformSide', () => {
   describe('IC 43 train HKI-PM', () => {
     const train = trainIc143DirectionReversesAfterTpe as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'right', // Helsinki - 8
-      'left', // Pasila - 3
-      'right', // Tikkurila - 2
-      'left', // Tampere - 3
-      'right', // Orivesi - 2
-      'right', // Jämsä - 2
-      'left', // Jyväskylä - 1
-      'right', // Hankasalmi - 2
-      'right', // Pieksämäki - 3
+      StationPlatformSide.Right, // Helsinki - 8
+      StationPlatformSide.Left, // Pasila - 3
+      StationPlatformSide.Right, // Tikkurila - 2
+      StationPlatformSide.Left, // Tampere - 3
+      StationPlatformSide.Right, // Orivesi - 2
+      StationPlatformSide.Right, // Jämsä - 2
+      StationPlatformSide.Left, // Jyväskylä - 1
+      StationPlatformSide.Right, // Hankasalmi - 2
+      StationPlatformSide.Right, // Pieksämäki - 3
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
@@ -152,30 +155,30 @@ describe('getTrainPlatformSide', () => {
   describe('PYO 276 HLI-HKI', () => {
     const train = trainPyo276KliHki as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'right', // Kolari - 1
-      'right', // Pello - 1
-      'right', // Ylitornio - 1
-      'right', // Tornio-Itäinen - 1
+      StationPlatformSide.Right, // Kolari - 1
+      StationPlatformSide.Right, // Pello - 1
+      StationPlatformSide.Right, // Ylitornio - 1
+      StationPlatformSide.Right, // Tornio-Itäinen - 1
       null, // Laurila - 621 - TODO: No data
-      'right', // Kemi - 1
+      StationPlatformSide.Right, // Kemi - 1
       null, // Simo - 421 - TODO: No data
-      'right', // Oulu - 3
-      'right', // Kempele - 1
+      StationPlatformSide.Right, // Oulu - 3
+      StationPlatformSide.Right, // Kempele - 1
       null, // Hirvineva - 601 - TODO: No data
-      'right', // Ylivieska - 1
-      'left', // Kokkola - 2
-      'right', // Pännäinen - 1
+      StationPlatformSide.Right, // Ylivieska - 1
+      StationPlatformSide.Left, // Kokkola - 2
+      StationPlatformSide.Right, // Pännäinen - 1
       null, // Kauhava - 002 - TODO: No data
       null, // Seinäjoki - 1 - TODO: No data
       null, // Ratikylä - 602 - TODO: No data
-      'right', // Tampere - 1
-      'left', // Toijala - 3
-      'left', // Hämeenlinna - 2
-      'right', // Riihimäki - 1
-      'right', // Tikkurila - 1
+      StationPlatformSide.Right, // Tampere - 1
+      StationPlatformSide.Left, // Toijala - 3
+      StationPlatformSide.Left, // Hämeenlinna - 2
+      StationPlatformSide.Right, // Riihimäki - 1
+      StationPlatformSide.Right, // Tikkurila - 1
       null, // Pasila tavara - 101 - TODO: No data
-      'left', // Pasila - 6
-      'left', // Helsinki - 8
+      StationPlatformSide.Left, // Pasila - 6
+      StationPlatformSide.Left, // Helsinki - 8
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
@@ -184,32 +187,32 @@ describe('getTrainPlatformSide', () => {
   describe('IC 266 ROI-HKI', () => {
     const train = trainIc266RoiHki as TrainDetailsFragment;
     const expectedPlatformSides = [
-      'right', // Rovaniemi - 1
-      'left', // Muurola - 1
-      'left', // Tervola - 1
-      'right', // Kemi - 1
-      'right', // Oulu - 3
-      'right', // Kempele - 1
+      StationPlatformSide.Right, // Rovaniemi - 1
+      StationPlatformSide.Left, // Muurola - 1
+      StationPlatformSide.Left, // Tervola - 1
+      StationPlatformSide.Right, // Kemi - 1
+      StationPlatformSide.Right, // Oulu - 3
+      StationPlatformSide.Right, // Kempele - 1
       null, // Hirvineva - 601 - TODO: No data
-      'right', // Ruukki - 1
-      'right', // Vihanti - 2
-      'left', // Oulainen - 2 - Note: Verified. The map shows track numbers in slightly wrong position.
-      'right', // Ylivieska - 1
-      'right', // Kannus - 1
-      'left', // Kokkola - 2
-      'left', // Pännäinen - 2
-      'left', // Kauhava - 1
-      'right', // Lapua - 1
+      StationPlatformSide.Right, // Ruukki - 1
+      StationPlatformSide.Right, // Vihanti - 2
+      StationPlatformSide.Left, // Oulainen - 2 - Note: Verified. The map shows track numbers in slightly wrong position.
+      StationPlatformSide.Right, // Ylivieska - 1
+      StationPlatformSide.Right, // Kannus - 1
+      StationPlatformSide.Left, // Kokkola - 2
+      StationPlatformSide.Left, // Pännäinen - 2
+      StationPlatformSide.Left, // Kauhava - 1
+      StationPlatformSide.Right, // Lapua - 1
       null, // Seinäjoki - 2 - TODO: No data
-      'right', // Parkano - 1
-      'right', // Tampere - 3
-      'left', // Toijala - 3
-      'left', // Hämeenlinna - 2
-      'right', // Riihimäki - 1
-      'right', // Tikkurila - 1
+      StationPlatformSide.Right, // Parkano - 1
+      StationPlatformSide.Right, // Tampere - 3
+      StationPlatformSide.Left, // Toijala - 3
+      StationPlatformSide.Left, // Hämeenlinna - 2
+      StationPlatformSide.Right, // Riihimäki - 1
+      StationPlatformSide.Right, // Tikkurila - 1
       null, // Pasila tavara - 101 - TODO: No data
-      'left', // Pasila - 6
-      'left', // Helsinki - 8
+      StationPlatformSide.Left, // Pasila - 6
+      StationPlatformSide.Left, // Helsinki - 8
     ];
 
     testStationPlatformSides(train, expectedPlatformSides);
