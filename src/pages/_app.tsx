ea4254e12a4b2ff47300836b82f9b5ad2f97a707
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2023-08-17 17:58:59 +0300

    wip: Add default meta tags to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import Footer from '../components/Footer';
 import ServiceWorkerUpdatePrompt from '../components/ServiceWorkerUpdatePrompt';
@@ -13,6 +14,29 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
       <div className="App" data-testid="app">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="color-scheme" content="light dark" />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#f9f9fb"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#1e1e1e"
+          />
+          <title>Junien aikataulut ja sijainnit | Junaan.fi</title>
+          <meta
+            name="description"
+            content="Junien aikataulut, sijainnit, ja kokoonpanot reaaliajassa"
+          />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
         <ServiceWorkerUpdatePrompt />
         <SwitchLanguage />
         <TopNavBar />
