5039d70b9daa33efb8e6daa36b4d6e5fd1df6a62
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2023-08-20 12:44:52 +0300

    wip: Remove unused packages

--- src/utils/mqttPatch.ts
+++
@@ -1,8 +0,0 @@
-import buffer from 'buffer';
-import process from 'process';
-
-// Hack to get mqtt package work with Webpack 5
-(window as any).Buffer = buffer.Buffer;
-(window as any).process = process;
-
-export {};
