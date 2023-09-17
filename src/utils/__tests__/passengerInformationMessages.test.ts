6b803dee8599267ec294feba95f13bbb2f7c8d73
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2023-09-17 14:53:28 +0300

    Reduce code duplication in passengerInformationMessages.test.ts

--- src/utils/__tests__/passengerInformationMessages.test.ts
+++ src/utils/__tests__/passengerInformationMessages.test.ts
@@ -64,16 +64,26 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
     jest.useFakeTimers();
   });
 
+  const passengerInformationMessageBase: PassengerInformationMessage = {
+    id: 'SHM20220818174358380',
+    version: 94,
+    creationDateTime: '2023-09-10T14:37:00Z',
+    startValidity: '2023-09-09T21:00:00Z',
+    endValidity: '2023-09-10T20:59:00Z',
+    stations: ['HKI'],
+  };
+
+  const defaultPassengerInformationTextContent = {
+    fi: 'Huomio!',
+    sv: 'Observera!',
+    en: 'Attention!',
+  };
+
   describe('text content modifications', () => {
     it('should add junaan.fi text to both audio and video text content if the text contains junalahdot.fi', () => {
       const passengerInformationMessages: PassengerInformationMessage[] = [
         {
-          id: 'SHM20220818174358380',
-          version: 94,
-          creationDateTime: '2023-09-10T14:37:00Z',
-          startValidity: '2023-09-09T21:00:00Z',
-          endValidity: '2023-09-10T20:59:00Z',
-          stations: ['HKI'],
+          ...passengerInformationMessageBase,
           audio: {
             text: {
               fi: 'Huomio! junalahdot.fi',
@@ -117,18 +127,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
     it('should not return messages with unsupported deliveryRules type', () => {
       const passengerInformationMessages: PassengerInformationMessage[] = [
         {
-          id: 'SHM20220818174358380',
-          version: 94,
-          creationDateTime: '2023-09-10T14:37:00Z',
-          startValidity: '2023-09-09T21:00:00Z',
-          endValidity: '2023-09-10T20:59:00Z',
-          stations: ['HKI'],
+          ...passengerInformationMessageBase,
           audio: {
-            text: {
-              fi: 'Huomio!',
-              sv: 'Observera!',
-              en: 'Attention!',
-            },
+            text: defaultPassengerInformationTextContent,
             deliveryRules: {
               deliveryType: 'NO_SUPPORT_FOR_SUCH_TYPE' as any,
             },
@@ -146,18 +147,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
     it('should return messages that have no deliveryRules', () => {
       const passengerInformationMessages: PassengerInformationMessage[] = [
         {
-          id: 'SHM20220818174358380',
-          version: 94,
-          creationDateTime: '2023-09-10T14:37:00Z',
-          startValidity: '2023-09-09T21:00:00Z',
-          endValidity: '2023-09-10T20:59:00Z',
-          stations: ['HKI'],
+          ...passengerInformationMessageBase,
           audio: {
-            text: {
-              fi: 'Huomio!',
-              sv: 'Observera!',
-              en: 'Attention!',
-            },
+            text: defaultPassengerInformationTextContent,
           },
         },
       ];
@@ -173,22 +165,12 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       describe('message with creationDateTime "2023-09-10T14:37:00Z"', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20220818174358380',
-            version: 94,
+            ...passengerInformationMessageBase,
             creationDateTime: '2023-09-10T14:37:00Z',
-            startValidity: '2023-09-09T21:00:00Z',
-            endValidity: '2023-09-10T20:59:00Z',
-            stations: ['HKI'],
             audio: {
-              text: {
-                fi: 'Huomio! Intercity 963 klo 17.36 Kupittaalle saapuu lähtöraiteelleen noin 5 minuutin kuluttua.',
-                sv: 'Observera! Intercity 963 till Kuppis kl 17.36 anländer till sitt avgångsspår om cirka 5 minuter.',
-                en: 'Attention! Intercity 963 to Kupittaa at 17.36 arrives to its departure track in about 5 minutes.',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 deliveryType: 'NOW',
-                repetitions: 0,
-                repeatEvery: 0,
               },
             },
           },
@@ -228,18 +210,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       it('should return messages with no deliveryAt specified', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20220818174358380',
-            version: 94,
-            creationDateTime: '2023-09-10T14:37:00Z',
-            startValidity: '2023-09-09T21:00:00Z',
-            endValidity: '2023-09-10T20:59:00Z',
-            stations: ['HKI'],
+            ...passengerInformationMessageBase,
             audio: {
-              text: {
-                fi: 'Huomio!',
-                sv: 'Observera!',
-                en: 'Attention!',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 deliveryType: 'DELIVERY_AT',
               },
@@ -258,18 +231,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       describe('message with deliveryAt "2023-09-10T14:30:00Z"', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20220818174358380',
-            version: 94,
-            creationDateTime: '2023-09-10T14:37:00Z',
-            startValidity: '2023-09-09T21:00:00Z',
-            endValidity: '2023-09-10T20:59:00Z',
-            stations: ['HKI'],
+            ...passengerInformationMessageBase,
             audio: {
-              text: {
-                fi: 'Huomio! Intercity 963 klo 17.36 Kupittaalle saapuu lähtöraiteelleen noin 5 minuutin kuluttua.',
-                sv: 'Observera! Intercity 963 till Kuppis kl 17.36 anländer till sitt avgångsspår om cirka 5 minuter.',
-                en: 'Attention! Intercity 963 to Kupittaa at 17.36 arrives to its departure track in about 5 minutes.',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 deliveryType: 'DELIVERY_AT',
                 deliveryAt: '2023-09-10T14:30:00Z',
@@ -312,18 +276,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       describe('message with timespan "2023-09-01T08:20:00+03:00" to "2023-09-03T15:00:00+03:00" on all weekdays', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20230420115013317',
-            version: 15,
-            creationDateTime: '2023-09-03T08:07:00Z',
-            startValidity: '2023-09-01T21:00:00Z',
-            endValidity: '2023-09-03T20:59:00Z',
-            stations: ['KV'],
+            ...passengerInformationMessageBase,
             audio: {
-              text: {
-                fi: 'Hyvät matkustajat! Rataverkolla tehdään kunnossapitotöitä. 2.-3.9.2023 osa junista Kouvolan ja Pieksämäen, sekä Kouvolan ja Joensuun välillä, on korvattu busseilla. Lisäksi joulukuuhun asti, myös Kouvolan ja Kotkan sataman välillä osa junista on korvattu busseilla. Kouvolassa korvaavat bussit lähtevät asemarakennuksen ja raiteen 1 välistä. Korvatut junavuorot, bussien aikataulut sekä asemakohtaiset lähtöpaikat löydät osoitteesta vr.fi.',
-                sv: 'Bästa passagerare! Underhållsarbeten pågår på järnvägsnätet. Mellan 2 till 3 september 2023 har en del av tågen mellan Kouvola och Pieksämäki samt Kouvola och Joensuu ersatts av bussar.  Dessutom fram till december ersätts några tåg med buss mellan Kouvola och Kotka. I Kouvola avgår ersättningsbussar mellan stationshuset och spår 1.  Ersatta tågtider, busstidtabeller och stationsspecifika avgångsplatser finns på vr.fi.',
-                en: 'Dear passangers! There are changes in train traffic due to maintenance works. Between 2.9.2023 and 3.9.2023, some of the trains between Kouvola and Pieksämäki, and Kouvola and Joensuu, have been replaced by buses. There are also some train replaced by buses until December between Kouvola, and Kotka. In Kouvola, replacement buses leave from between the station building and track 1. Replaced train times, bus schedules and station-specific departure points can be found at vr.fi.',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 startDateTime: '2023-09-01T21:00:00Z',
                 endDateTime: '2023-09-03T20:59:00Z',
@@ -380,20 +335,12 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       describe('train with departureDate 2023-09-02 and message with stations [LUS=15:51, PUN=16:00]', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'MVM20230902154021600',
-            version: 5,
-            creationDateTime: '2023-09-02T15:51:00Z',
-            startValidity: '2023-09-02T15:51:00Z',
-            endValidity: '2023-09-04T00:00:00Z',
+            ...passengerInformationMessageBase,
             trainNumber: 750,
             trainDepartureDate: '2023-09-02',
             stations: ['LUS', 'PUN', 'REE', 'KIÄ', 'PKY'],
             audio: {
-              text: {
-                fi: 'Huomio ilmoitus! Juna H750 on korvattu linja-autolla Savonlinnan ja Parikkalan välillä. Korvaava linja-auto lähtee aseman edustalta. Bussin matka-aika asemien välillä on pidempi kuin junan matka-aika. Info:vr.fi',
-                sv: 'Observera! Tåg H750 har ersatts av ett buss mellan Nyslott och Parikkala. Bussen avgår från järnvägsstationen.  Restiden för bussen mellan stationerna är längre än tågets restid. Info:vr.fi',
-                en: 'Attention! Train H750 has been replaced by bus between Savonlinna and Parikkala. The bus leaves from the railwaystation. The bus travel time between stations is longer than the train travel time. Info:vr.fi.',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 deliveryType: 'ON_SCHEDULE',
                 repetitions: 1,
@@ -493,18 +440,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       it('should not return messages with type ON_EVENT because they are not currently supported', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20220818174358380',
-            version: 94,
-            creationDateTime: '2023-09-10T14:37:00Z',
-            startValidity: '2023-09-09T21:00:00Z',
-            endValidity: '2023-09-10T20:59:00Z',
-            stations: ['HKI'],
+            ...passengerInformationMessageBase,
             audio: {
-              text: {
-                fi: 'Huomio!',
-                sv: 'Observera!',
-                en: 'Attention!',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 deliveryType: 'ON_EVENT',
               },
@@ -526,18 +464,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
     it('should not return messages with unsupported deliveryRules type', () => {
       const passengerInformationMessages: PassengerInformationMessage[] = [
         {
-          id: 'SHM20220818174358380',
-          version: 94,
-          creationDateTime: '2023-09-10T14:37:00Z',
-          startValidity: '2023-09-09T21:00:00Z',
-          endValidity: '2023-09-10T20:59:00Z',
-          stations: ['HKI'],
+          ...passengerInformationMessageBase,
           video: {
-            text: {
-              fi: 'Huomio!',
-              sv: 'Observera!',
-              en: 'Attention!',
-            },
+            text: defaultPassengerInformationTextContent,
             deliveryRules: {
               deliveryType: 'NO_SUPPORT_FOR_SUCH_TYPE' as any,
             },
@@ -555,18 +484,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
     it('should return messages that have no deliveryRules', () => {
       const passengerInformationMessages: PassengerInformationMessage[] = [
         {
-          id: 'SHM20220818174358380',
-          version: 94,
-          creationDateTime: '2023-09-10T14:37:00Z',
-          startValidity: '2023-09-09T21:00:00Z',
-          endValidity: '2023-09-10T20:59:00Z',
-          stations: ['HKI'],
+          ...passengerInformationMessageBase,
           video: {
-            text: {
-              fi: 'Huomio!',
-              sv: 'Observera!',
-              en: 'Attention!',
-            },
+            text: defaultPassengerInformationTextContent,
           },
         },
       ];
@@ -582,18 +502,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       describe('message with timespan "2023-09-11T01:00:00+03:00" to "2023-09-15T04:20:00+03:00" on Mon-Thu', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20230727799106000',
-            version: 7,
-            creationDateTime: '2023-09-08T04:16:00Z',
-            startValidity: '2023-09-10T21:00:00Z',
-            endValidity: '2023-09-15T20:59:00Z',
-            stations: ['AVP', 'VEH', 'KTÖ'],
+            ...passengerInformationMessageBase,
             video: {
-              text: {
-                fi: 'Suomeksi',
-                sv: 'På svenska',
-                en: 'In English',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 startDateTime: '2023-09-10T21:00:00Z',
                 endDateTime: '2023-09-15T20:59:00Z',
@@ -644,18 +555,9 @@ describe('getPassengerInformationMessagesCurrentlyRelevant', () => {
       describe('message with timespan "2023-09-08T21:00:00Z" to "2023-09-10T20:59:00Z" on Mon-Sun from 7:45 to 7:50', () => {
         const passengerInformationMessages: PassengerInformationMessage[] = [
           {
-            id: 'SHM20230504174327890',
-            version: 10,
-            creationDateTime: '2023-09-09T05:38:00Z',
-            startValidity: '2023-09-08T21:00:00Z',
-            endValidity: '2023-09-10T20:59:00Z',
-            stations: ['HKI'],
+            ...passengerInformationMessageBase,
             video: {
-              text: {
-                fi: 'Raide suljettu. Varmista junasi lähtöraide aikataulunäytöiltä tai osoitteesta junalahdot.fi.',
-                sv: 'Spåret är stängt. Kolla tågens avgångsspår från tidtabellsskärmarna eller på addressen junalahdot.fi.',
-                en: 'The track is closed. Check the departure tracks from the timetable displays or at junalahdot.fi.',
-              },
+              text: defaultPassengerInformationTextContent,
               deliveryRules: {
                 startDateTime: '2023-09-08T21:00:00Z',
                 endDateTime: '2023-09-10T20:59:00Z',
