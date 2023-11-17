322d958a9fe04ba7044b0aba593b9503ee98cb67
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2023-11-17 16:47:18 +0200

    Adjust stopping sectors color

--- src/components/TrainComposition.tsx
+++ src/components/TrainComposition.tsx
@@ -263,15 +263,18 @@ function TrainComposition({
       {stationName && (
         <Box
           className="stopping-sectors"
-          sx={{
+          sx={(theme) => ({
             display: 'flex',
             div: {
               border: '1px solid transparent',
-              backgroundColor: '#e5e5e5',
+              backgroundColor:
+                theme.palette.mode === 'light'
+                  ? 'divider'
+                  : theme.palette.grey[400],
               color: 'black',
               backgroundClip: 'padding-box',
             },
-          }}
+          })}
         >
           <Box sx={{ flex: 1 }}>A</Box>
           <Box sx={{ flex: 0.8 }}>B</Box>
