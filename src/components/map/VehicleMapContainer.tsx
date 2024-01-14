e351e983443da63aa508d3be137fa9c401fbeaf5
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2024-01-14 16:37:51 +0200

    Simplify condition that has redundant check

--- src/components/map/VehicleMapContainer.tsx
+++ src/components/map/VehicleMapContainer.tsx
@@ -88,10 +88,7 @@ const VehicleMapContainer = ({
         // the maplibre-gl Map may have wrong initial canvas size as the Map determines
         // its dimensions from the container element's clientWidth/clientHeight which
         // would be 0 when the container is detached from DOM.
-        if (
-          map &&
-          map.getContainer().clientHeight !== map.getCanvas().clientHeight
-        ) {
+        if (map.getContainer().clientHeight !== map.getCanvas().clientHeight) {
           map.resize();
         }
       }
