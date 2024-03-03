0f7449acd6751e9299e8f1586d9aa6da87777f6d
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2024-03-03 20:00:32 +0200

    Make getTimeTableRowForStation return latest row as fallback
    Includes other refactoring improvements and adds tests for getTimeTableRowForStation utility function.

--- src/utils/getTimeTableRowForStation.ts
+++ src/utils/getTimeTableRowForStation.ts
@@ -6,20 +6,41 @@ import {
   TrainTimeTableRowFragment,
 } from '../graphql/generated/digitraffic';
 
+import { isDefined } from './common';
+
 export default function getTimeTableRowForStation(
   stationShortCode: string,
   train: TrainByStationFragment,
   type: TimeTableRowType
 ): TrainTimeTableRowFragment | undefined | null {
-  // Note that there might exist multiple time table rows of same type for same station
-  // for e.g. on Kehärata P and I trains run on a loop from Helsinki to Helsinki
-  // Therefore we want to find the time table row that is nearest in the future
+  // Filter time table rows that match stationShortCode and type
+  let matchingRows = train.timeTableRows
+    ?.filter(
+      (r) => r?.station?.shortCode === stationShortCode && r?.type === type
+    )
+    .filter(isDefined);
+
+  if (!matchingRows || matchingRows.length === 0) {
+    return undefined;
+  } else if (matchingRows.length === 1) {
+    return matchingRows[0];
+  }
+
+  // There might exist multiple time table rows of same type for same station.
+  // For example on Kehärata P and I trains run on a loop from Helsinki to Helsinki.
+  // For these cases we want to find the time table row that is next in the future -
+  // 10 mins if such exists, and otherwise the latest.
+
   const now = addMinutes(new Date(), -10);
-  return train.timeTableRows?.find(
-    (r) =>
-      r?.station?.shortCode === stationShortCode &&
-      r?.type === type &&
-      (now < parseISO(r.scheduledTime) ||
-        (r?.liveEstimateTime && now < parseISO(r.liveEstimateTime)))
-  );
+  for (const row of matchingRows) {
+    if (
+      now < parseISO(row.scheduledTime) ||
+      (row.liveEstimateTime && now < parseISO(row.liveEstimateTime))
+    ) {
+      return row; // Return the nearest future - 10 mins time table row
+    }
+  }
+
+  // Return latest (time table rows should be already sorted in ascending order)
+  return matchingRows[matchingRows.length - 1];
 }
