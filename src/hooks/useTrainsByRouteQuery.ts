742b6dcedef20ead6380fe10d0c8db68c9a2f93d
Vili Ketonen <25618592+viliket@users.noreply.github.com>
2024-03-12 19:43:17 +0200

    Add missing useTrainsByRouteQuery hook

---
+++ src/hooks/useTrainsByRouteQuery.ts
@@ -0,0 +1,44 @@
+import { queryOptions, useQuery } from '@tanstack/react-query';
+import { addDays, format, subDays } from 'date-fns';
+
+import { digitrafficClient } from '../graphql/client';
+import {
+  TimeTableRowType,
+  TrainsByRouteDocument,
+} from '../graphql/generated/digitraffic/graphql';
+
+const trainsByRouteQuery = (
+  deptOrArrStationCodeFilter: string | null,
+  stationCode: string | undefined,
+  timeTableType: TimeTableRowType
+) => {
+  const departureStation =
+    timeTableType === TimeTableRowType.Departure
+      ? stationCode
+      : deptOrArrStationCodeFilter;
+  const arrivalStation =
+    timeTableType === TimeTableRowType.Departure
+      ? deptOrArrStationCodeFilter
+      : stationCode;
+  return queryOptions({
+    queryKey: ['trainsByRouteQuery', stationCode],
+    queryFn: () =>
+      digitrafficClient.request(TrainsByRouteDocument, {
+        departureStation: departureStation ?? '',
+        arrivalStation: arrivalStation ?? '',
+        departureDateGreaterThan: format(subDays(new Date(), 1), 'yyyy-MM-dd'),
+        departureDateLessThan: format(addDays(new Date(), 2), 'yyyy-MM-dd'),
+      }),
+    enabled: Boolean(departureStation && arrivalStation),
+    refetchInterval: 10000,
+  });
+};
+
+export const useTrainsByRouteQuery = (
+  deptOrArrStationCodeFilter: string | null,
+  stationCode: string | undefined,
+  timeTableType: TimeTableRowType
+) =>
+  useQuery(
+    trainsByRouteQuery(deptOrArrStationCodeFilter, stationCode, timeTableType)
+  );
